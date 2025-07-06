59624b631131cc4a118b1e5cb892309d1a85d330
derksKCodes <derksthetribal.ghub@gmail.com>
2025-07-06 17:17:17 +0300

    updated project files

--- config.py
+++ config.py
@@ -62,7 +62,3 @@ class Config:
         }
     }
 
-print("DB_HOST:", os.getenv('DB_HOST'))
-print("DB_USER:", os.getenv('DB_USER'))
-print("DB_PASSWORD:", os.getenv('DB_PASSWORD'))
-print("DB_NAME:", os.getenv('DB_NAME'))
